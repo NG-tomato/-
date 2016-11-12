57087b49300513a4198ac537e712aacb7de5739d
NG_tomato <tomatodakewayurusanai@gmail.com>
2016-11-12 17:31:58 +0900

    olearn, forward, printの3つのメソッドを完成

--- 機械学習/neural_network.java
+++ 機械学習/neural_network.java
@@ -61,7 +61,7 @@ public class neural_network{
 	
 	/*
 	学習データの読み込みをするメソッド
-	getdata(学習データを入れる配列)
+	getdata(学習データの配列)
 	*/
 	public static int getdata(double e[][]){
 		int n_of_e =0;	// データセットの個数
@@ -79,12 +79,72 @@ public class neural_network{
 	}
 	
 	
-	
+	/*
+	出力層の重みを学習するメソッド
+	olearn(出力層の重みの配列, 中間層の出力の配列, 学習データの配列, 出力)
+	*/
 	public static void olearn(double wo[] ,double hi[] ,double e[] ,double o){
+		int i;		// 繰り返しの制御
+		double d;	// 重み計算に利用
+		
+		d = (e [INPUTNO] - o) * o * (1 - o);	// 誤差の計算
+		for(i=0; i < HIDDENNO; ++i){
+			wo[i] += ALPHA*hi[i] * d;			// 結合荷重の学習
+		}
+		wo[i] += ALPHA * (-1.0) * d;			// しきい値の学習
 	}
+	
+	
+	/*
+	順方向の計算をするメソッド
+	forward(中間層の重みの配列, 出力層の重みの配列, 中間層の出力の配列, 学習データの配列)
+	*/
 	public static double forward(double wh[][] ,double wo[], double hi[], double e[]){
+		int j,i;		// 繰り返しの制御
+		double u;		// 重み付き和の合計
+		double o;		// 出力の計算
+		
+		// hiの計算
+		for(i = 0; i < HIDDENNO; i++){
+			u=0;						// 重み付き和を求める
+			for(j = 0; j < INPUTNO; ++j){
+				u += e[j] * wh[i][j];
+			}
+			u -= wh [i][j];		// しきい値の処理
+			hi[i] = s(u);
+		}
+		
+		//出力oの計算
+		o = 0;
+		for(i = 0;i < HIDDENNO; i++){
+			o += hi[i] * wo[i];
+		}
+		o -= wo[i];		// しきい値の処理
+		
+		return s(o);
 	}
-	public static void print(double wh[][], double wo[]){}
+	
+	
+	/*
+	結果の出力
+	print(中間層の重みの配列, 出力層の重みの配列)
+	*/
+	public static void print(double wh[][], double wo[]){
+		int i,j;
+		
+		for(i = 0; i < HIDDENNO; ++i){
+			for(j = 0; j < INPUTNO; ++j){
+				System.out.print(wh[i][j] + " ");
+			}
+			System.out.println();
+		}
+		for(i = 0; i < HIDDENNO + 1; ++i){
+			System.out.print(wo[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	
 	public static void initwh(double wh[][]){}
 	public static void initwo(double wo[]){}
 	public static double drnd(){}
